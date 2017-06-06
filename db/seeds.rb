e47c4b8693cddcd4f3a276bfee581e244f2bcc58
wangxing <18912540921@163.com>
2017-06-06 15:45:52 +0800

    session debug

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,8 @@ if User.find_by(email: "admin@test.com").nil?
   u = User.new
   u.username = "test"
   u.email = "wx@test.com"           # 可以改成自己的 email
-  u.password = "123456"                # 最少要六码
-  u.password_confirmation = "123456"   # 最少要六码
+  u.password = "1234567"                # 最少要六码
+  u.password_confirmation = "1234568"   # 最少要六码
   u.is_admin = true
   u.save
   puts "Admin 已经建立好了，帐号为#{u.email}, 密码为#{u.password}"
